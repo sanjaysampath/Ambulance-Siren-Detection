bcfcb869190571a6c57eab15304bbd126680f71e
Sanjay Sampath <52173352+sanjaysampath@users.noreply.github.com>
2021-09-30 11:16:06 +0530

    Update Matlab_with_SVM.m

--- Matlab_with_SVM.m
+++ Matlab_with_SVM.m
@@ -1,40 +1,45 @@
+% training model
 clc; close all; clear all;
- 
-folder = 'C:\Users\Sanjay Sampath\Desktop\Programming\Matlab\ambulance_dataset\ambulance\';
-audio_files = dir(fullfile(folder,'*.wav'));
-[audio1,fs] = audioread('C:\Users\Sanjay Sampath\Desktop\Programming\Matlab\ambulance_dataset\ambulance\sound_1.wav');
-   audio_mfcc = mfcc(audio1(:,1),fs);
-   rms_audio = rms(audio_mfcc);
-for k=1:numel(audio_files)-1
-   [audio1,fs] = audioread(strcat(folder,audio_files(k+1).name));
-   audio_mfcc=mfcc(audio1(:,1),fs);
 
-    rms_audio_1 = (rms(audio_mfcc));
-    rms_audio = [rms_audio;rms_audio_1];
+%............1)Reading all Audio Signals and finding their MFCC............
+
+
+folder = 'C:\Users\Sanjay Sampath\Desktop\Programming\Matlab\Final_Datasets\Ambulance\';
+audio_files = dir(fullfile(folder,'*.wav'));
+var_audio = [];
+
+for k=1:numel(audio_files)      % reads all the ambulance audio files
+   [audio1,fs] = audioread(strcat(folder,audio_files(k).name));
+   audio_mfcc=mfcc(audio1(:,1),fs);
+   var_audio_1 = (rms(audio_mfcc));        %finds rms of ambulance signals
+   var_audio = [var_audio;var_audio_1];
 end
 
-folder = 'C:\Users\Sanjay Sampath\Desktop\Programming\Matlab\ambulance_dataset\firetruck\';
+folder = 'C:\Users\Sanjay Sampath\Desktop\Programming\Matlab\Final_Datasets\Not_ambulance\';
 audio_files = dir(fullfile(folder,'*.wav'));
-[audio1,fs] = audioread('C:\Users\Sanjay Sampath\Desktop\Programming\Matlab\ambulance_dataset\firetruck\dog1.wav');
-   audio_mfcc = mfcc(audio1(:,1),fs);
-   rms_audio_not = rms(audio_mfcc);
-for k=1:numel(audio_files)-1
-   [audio1,fs] = audioread(strcat(folder,audio_files(k+1).name));
+var_audio_not = [];
+
+for k=1:numel(audio_files)       % reads all the non-ambulance audio files
+   [audio1,fs] = audioread(strcat(folder,audio_files(k).name));
    audio_mfcc=mfcc(audio1(:,1),fs);
-
-    rms_audio_1 = (rms(audio_mfcc));
-    rms_audio_not = [rms_audio_not;rms_audio_1];
+   var_audio_1 = (rms(audio_mfcc));        %finds rms of non-ambulance signals
+   var_audio_not = [var_audio_not;var_audio_1];
 end
-x = [rms_audio ; rms_audio_not];
-y = [ones(200,1) ; zeros(401,1)];
 
-rand_num =randperm(601);
 
-x_train = x(rand_num(1:500),:);
-y_train = y(rand_num(1:500));
+%...........................2)Training the Model...........................
 
-x_test = x(rand_num(501:end),:);
-y_test = y(rand_num(501:end));
+
+x = [var_audio ; var_audio_not];
+y = [ones(800,1) ; zeros(1000,1)];
+
+rand_num =randperm(1800);
+
+x_train = x(rand_num(1:1440),:);
+y_train = y(rand_num(1:1440));
+
+x_test = x(rand_num(1441:end),:);
+y_test = y(rand_num(1441:end));
 
 c = cvpartition(y_train,'k',5);
 opts = statset('display','iter');
@@ -42,7 +47,7 @@ opts = statset('display','iter');
 fun =@(train_data, train_labels, test_data, test_labels)... 
  sum(predict(fitcsvm(train_data, train_labels,'KernelFunction','rbf'), test_data) ~= test_labels);
 
-[f1, history] = sequentialfs(fun,x_train,y_train,'cv',c, 'options' ,opts);
+[f1, history] = sequentialfs(fun,x_train,y_train,'cv',c, 'options' ,opts, 'nfeatures', 14);
 
 x_train_with_best = x_train(:,f1);
 Md1 = fitcsvm(x_train_with_best,y_train,'KernelFunction','rbf','OptimizeHyperparameters','auto',...
@@ -50,22 +55,68 @@ Md1 = fitcsvm(x_train_with_best,y_train,'KernelFunction','rbf','OptimizeHyperpar
       'expected-improvement-plus','ShowPlots',true));
   
 x_test_with_best = x_test(:,f1);
-sum(predict(Md1,x_test_with_best) == y_test)/length(y_test) * 100
+accuracy = sum(predict(Md1,x_test_with_best) == y_test)/length(y_test) * 100
+
+save Md1;
+
+
+%...........................3)Real time testing............................
 
 
 
-[audio1,fs] = audioread('C:\Users\Sanjay Sampath\Desktop\Programming\Matlab\Test\test_audio7.wav');
-[coeffs1] = mfcc(audio1(:,1),fs);
-rms_test= rms(coeffs1);
+clc; close all; clear all;
 
+load Md1;
 
-% [audio1,fs] = audioread('C:\Users\Sanjay Sampath\Desktop\Programming\Matlab\Test\dog2.wav');
-% [coeffs1] = mfcc(audio1(:,1),fs);
-% real_test = rms(coeffs1);
-% 
-% x_real_testing_final = real_test(:,f1);
-% predict(Md1,x_real_testing_final)
+instrhwinfo('Bluetooth','HC-05');
+bt = Bluetooth('HC-05', 1);
+fopen(bt);
+count_t = 0;
+count_f = 0;
 
+while true
+    recObj = audiorecorder(44100,16,1);
+    % disp("recording");
+    recordblocking(recObj, 3);
+    test_audio = getaudiodata(recObj);
+    % disp("end of recording");
+    filename = 'C:\Users\Sanjay Sampath\Desktop\Programming\Matlab\test_audio_record.wav';
+    audiowrite(filename, test_audio, 44100);
+    
+    [audioIn_test,fsTest] = audioread(filename);
+    [test_coeffs] = mfcc(audioIn_test(:,1),fsTest);
+    testAudio_rms = rms(test_coeffs);
+    
+    x_real_testing_final = testAudio_rms(:,f1);
+    b = predict(Md1,x_real_testing_final);
+    
+    if(b == 1)
+        
+        count_t = count_t + 1;
+        if ( count_t  < 3)
+            disp("Calibrating ambulance");
+        end
+        if ( count_t > 2)
+            fprintf(bt,1);
+            disp("ambulance");
+            count_f = 0;
+        end
+    else
+        
+        count_f = count_f + 1;
+        if ( count_f  < 3)
+            disp("Calibrating not an ambulance");
+        end
+        
+        if (count_f > 2 )
+            count_t = 0;
+            disp("not an ambulance");
+            fprintf(bt,0);
+        end
+        
+    end
+    
+end
 
 
 
